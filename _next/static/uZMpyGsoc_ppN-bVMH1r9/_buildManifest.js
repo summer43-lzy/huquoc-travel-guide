d6b49ca141f56d04664f402f0b4288b31d41e71f
summer43-lzy <summer43-lzy@users.noreply.github.com>
2026-06-03 05:09:03 +0000

    deploy: 5ad6991542af2fe4ac6ec2271b05a2232fe7ca9d

---
+++ _next/static/uZMpyGsoc_ppN-bVMH1r9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/huquoc-travel-guide//_next/:path+",
+        "destination": "/huquoc-travel-guide/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
